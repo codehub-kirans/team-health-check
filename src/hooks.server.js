ac08e2b4a71a87810ec2c324cb805ba5a38fdceb
Kiran S <codehub.kirans@gmail.com>
2023-05-12 16:46:13 +0530

    Implemented Pocketbase IP configurability support

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,15 @@
 import PocketBase from 'pocketbase';
+import { config } from 'dotenv';
 import { serializeNonPOJOs } from '$lib/assets/js/utils.js';
 
+// Load environment variables from .env file
+config();
+
+
 export const handle = async ({ event, resolve }) => {
-    event.locals.pb = new PocketBase('http://127.0.0.1:8090')
+    const pocketbaseIP = import.meta.env.VITE_POCKETBASE_IP || 'http://127.0.0.1:8090';
+    event.locals.pb = new PocketBase(pocketbaseIP);
+
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '')
 
     if (event.locals.pb.authStore.isValid) {
--- src/lib/assets/js/utils.js
+++ src/lib/assets/js/utils.js
@@ -1,7 +1,9 @@
+
 export const serializeNonPOJOs = (obj) => {
     return structuredClone(obj)
 }
 
 export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
-    return `http://localhost:8090/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
+    const pocketbaseIP = import.meta.env.VITE_POCKETBASE_IP || 'http://127.0.0.1:8090';
+    return `${pocketbaseIP}/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
 };
